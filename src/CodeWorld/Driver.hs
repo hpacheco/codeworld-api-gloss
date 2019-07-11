87f3fcb49f9569be1ffb73e9c0971c92748e6d11
Hugo Pacheco <hpacheco@gmail.com>
2019-07-11 19:15:22 +0100

    message

--- src/CodeWorld/Driver.hs
+++ src/CodeWorld/Driver.hs
@@ -143,7 +143,7 @@ foreign import javascript unsafe "printMessage($1,$2);"
     printMessage :: JSString -> JSString -> IO ()
 
 reportRuntimeMessage :: String -> IO ()
-reportRuntimeMessage str = printLogMessage ("log" :: JSString) (Data.JSString.unpack)
+reportRuntimeMessage str = printMessage ("log" :: JSString) (Data.JSString.unpack str)
 
 say :: Text -> IO ()
 say str = do
