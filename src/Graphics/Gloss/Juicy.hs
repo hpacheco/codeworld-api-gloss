24150a8fa6e3ef30c9ac185fceebaa78f1de4183
Hugo Pacheco <hpacheco@gmail.com>
2025-08-27 14:58:40 +0100

    Update Juicy.hs

--- src/Graphics/Gloss/Juicy.hs
+++ src/Graphics/Gloss/Juicy.hs
@@ -30,6 +30,39 @@ fromImageRGBA8 :: Juicy.Image Juicy.PixelRGBA8 -> IO Picture
 fromImageRGBA8 img@(Juicy.Image w h _) = do
     cwcanvas <- CW.fromJuicyImage img
     return $ Image w h $ CW.CanvasImg cwcanvas
+
+-- * to simulate Gloss BitmapSection field. we read bitmaps using JuicyPixels though
+
+data Rectangle = Rectangle	{ rectPos :: (Int, Int), rectSize :: (Int, Int)	}
+    deriving (Eq,Ord,Show,Enum,Bounded)
+data RowOrder = TopToBottom	| BottomToTop
+    deriving (Eq,Ord,Show,Enum,Bounded)
+
+bitmapSection :: RowORder -> Rectangle -> DynamicImage -> IO (Maybe Picture)
+bitmapSection order r img = do
+    Just pic <- fromDynamicImage $ cropDynamicImage order r
+    return pic
+
+cropImage :: Pixel a => RowOrder -> Rectangle -> Image a -> Image a
+cropImage TopToBottom = cropImageTopToBottom
+cropImage BottomToTop = cropImageBottomToTop
+
+cropImageTopToBottom :: Pixel a => Rectangle -> Image a -> Image a
+cropImageTopToBottom (Rectangle (x,y) (width,height)) img =
+  generateImage (\dx dy -> pixelAt img (clampX (x + dx)) (clampY (y + dy))) width height
+  where
+    clampX pos = max 0 (min (imageWidth img - 1) pos)
+    clampY pos = max 0 (min (imageHeight img - 1) pos)
+    
+cropImageBottomToTop :: Pixel a => Rectangle -> Image a -> Image a
+cropImageBottomToTop (Rectangle (x,y) (width,height)) img = cropImageTopToBottom (Rectangle (x,invertY y) (width,height)) img
+    where
+    invertY y = imageHeight img - y - height
+
+cropDynamicImage :: RowOrder -> Rectangle -> DynamicImage -> DynamicImage
+cropDynamicImage order rect dynImg =
+  dynamicPixelMap (cropImage order rect) dynImg
+
     
 
 
